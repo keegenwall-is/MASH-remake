b3c4037fe79cdbb03cc631e4ca2c819ec0afbaf2
keegenwall-is <keegen.wallis@gmail.com>
2024-02-25 11:21:59 +1100

    Scoring system complete
    both for current soldiers and total soldiers

--- Mash Remake/Assets/Scripts/SoldiersCarried.cs
+++ Mash Remake/Assets/Scripts/SoldiersCarried.cs
@@ -1,22 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SoldiersCarried : MonoBehaviour
 {
 
     public int noOfSol;
 
+    public ScoreCounter scoreCounterScript;
+
+    public GameObject canvas;
+
+    public Text currentSol;
+
+    public Animator anim;
+
+    public AudioSource audioP;
+
+    public AudioClip heliFullSFX;
+    public AudioClip heliNotFullSFX;
+
+    private bool isHeliFullSFXPlaying = false;
+
     // Start is called before the first frame update
     void Start()
     {
         noOfSol = 0;
+        canvas = GameObject.Find("Canvas");
+        scoreCounterScript = canvas.GetComponent<ScoreCounter>();
+        anim = GetComponent<Animator>();
+        //heliFullSFX = GetComponent<AudioClip>();
+        //heliNotFullSFX = GetComponent<AudioClip>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (noOfSol == 3)
+        {
+            if (!isHeliFullSFXPlaying)
+            {
+                anim.Play("HelicopterFullAnim");
+                audioP.clip = heliFullSFX;
+                audioP.loop = true;
+                audioP.Play();
+                isHeliFullSFXPlaying = true;
+            }
+        } else
+        {
+            if (isHeliFullSFXPlaying)
+            {
+                anim.Play("HelicopterMoveAnim");
+                audioP.clip = heliNotFullSFX;
+                audioP.loop = true;
+                audioP.Play();
+                isHeliFullSFXPlaying = false;
+            } 
+        }
     }
 
     void OnTriggerEnter2D(Collider2D collision)
@@ -24,13 +65,20 @@ public class SoldiersCarried : MonoBehaviour
         if (collision.gameObject.tag == "Soldier")
         {
             noOfSol += 1;
-            Debug.Log(noOfSol);
+            //Debug.Log(noOfSol);
+
+            currentSol.text = "Soldiers in Helicopter: " + noOfSol;
         }
 
         if (collision.gameObject.tag == "Hospital")
         {
+            // increase score
+            scoreCounterScript.IncreaseCounter(noOfSol);
+
+            // set noOfSol in heli back to 0
             noOfSol = 0;
-            Debug.Log("Heli empty");
+            //Debug.Log("Heli empty");
+            currentSol.text = "Soldiers in Helicopter: " + noOfSol;
         }
     }
 }
